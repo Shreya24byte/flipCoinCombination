760e68f8db177536260c709c79cf73cad94f3be0
Shreya24byte <shreyanavelkar24@gmail.com>
2020-09-26 09:57:53 +0530

    Add: control statement for multiple coin flips and dictionary to store results

--- coinFlipCombination.sh
+++ coinFlipCombination.sh
@@ -1,12 +1,32 @@
 #!/bin/bash -x
 echo "Welcome to Flip Coin Combination Problem";
 
-#Coin toss
-toss=$((RANDOM%2));
+#Variables
+heads=0;
+tails=0
 
-if [ $toss -eq 0 ];
-then
-	echo "Heads";
-else
-	echo "Tails";
-fi
+#Coin toss function
+function coinFlip() {
+	coin=$((RANDOM%2));
+        if [ $coin -eq 0 ];
+        then
+                heads=$((heads+1));
+	else
+                tails=$((tails+1));
+        fi
+}
+
+
+#Multiple coin toss
+for ((toss=0; toss<100; toss++));
+do
+	coinFlip;
+done
+
+#Store singlet combination in the dictionary
+declare -A result
+result[head]=$heads;
+result[tail]=$tails;
+
+echo "Percentage of occurence of heads is $heads %";
+echo "Percentage of occurence of tails is $tails %";
